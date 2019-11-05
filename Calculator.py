ff2118f60dc117492f3e085af70771a8f4731447
leeminkyu97 <bomblmk97@naver.com>
2019-11-05 10:32:03 +0900

    calc

--- Calculator.py
+++ Calculator.py
@@ -3,11 +3,13 @@ from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5.QtWidgets import QLineEdit, QToolButton
 from PyQt5.QtWidgets import QSizePolicy
 from PyQt5.QtWidgets import QLayout, QGridLayout
-from keypad import *
-from calcFunctions import *
+
+from keypad import numPadList, operatorList, constantList, functionList
+import calcFunctions
 
 
 class Button(QToolButton):
+
     def __init__(self, text, callback):
         super().__init__()
         self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
@@ -16,12 +18,13 @@ class Button(QToolButton):
 
     def sizeHint(self):
         size = super(Button, self).sizeHint()
-        size.setHeight(size.height()+20)
+        size.setHeight(size.height() + 20)
         size.setWidth(max(size.width(), size.height()))
         return size
 
 
 class Calculator(QWidget):
+
     def __init__(self, parent=None):
         super().__init__(parent)
 
@@ -38,99 +41,83 @@ class Calculator(QWidget):
         funcLayout = QGridLayout()
 
         buttonGroups = {
-            'num': {'buttons':numPadList, 'layout': numLayout, 'columns': 3},
+            'num': {'buttons': numPadList, 'layout': numLayout, 'columns': 3},
             'op': {'buttons': operatorList, 'layout': opLayout, 'columns': 2},
             'constants': {'buttons': constantList, 'layout': constLayout, 'columns': 1},
-            'functions': {'buttons': functionList, 'layout': funcLayout, 'columns': 1}
+            'functions': {'buttons': functionList, 'layout': funcLayout, 'columns': 1},
         }
 
         for label in buttonGroups.keys():
-            r, c = 0, 0
+            r = 0; c = 0
             buttonPad = buttonGroups[label]
             for btnText in buttonPad['buttons']:
                 button = Button(btnText, self.buttonClicked)
                 buttonPad['layout'].addWidget(button, r, c)
                 c += 1
                 if c >= buttonPad['columns']:
-                    c = 0
-                    r += 1
+                    c = 0; r += 1
 
         # Layout
         mainLayout = QGridLayout()
         mainLayout.setSizeConstraint(QLayout.SetFixedSize)
 
         mainLayout.addWidget(self.display, 0, 0, 1, 2)
-
         mainLayout.addLayout(numLayout, 1, 0)
-
         mainLayout.addLayout(opLayout, 1, 1)
-
         mainLayout.addLayout(constLayout, 2, 0)
-
         mainLayout.addLayout(funcLayout, 2, 1)
 
         self.setLayout(mainLayout)
 
         self.setWindowTitle("My Calculator")
 
+
     def buttonClicked(self):
+
+        if self.display.text() == 'Error!':
+            self.display.setText('')
+
         button = self.sender()
         key = button.text()
-
-        errorList = ['ZeroDivisionErr', 'IndexErr', 'OverFlowErr',
-                     'TypeErr', 'ValueErr', 'SyntaxErr', 'etcErr']
-
-        if self.display.text() in errorList:
-            self.display.setText('')  # after error
 
         if key == '=':
             try:
                 result = str(eval(self.display.text()))
-            except ZeroDivisionError:  # divided by 0
-                result = 'ZeroDivisionErr'
-            except IndexError:  # over/under indexing
-                result = 'IndexErr'
-            except OverflowError:  # overflow (could be memory err)
-                result = 'OverflowErr'
-            except TypeError:  # wrong arg (data type)
-                result = 'TypeErr'
-            except ValueError:  # wrong arg (ex. big number)
-                result = 'ValueErr'
-            except SyntaxError:
-                result = 'SyntaxErr'
             except:
-                result = 'etcErr'
+                result = 'Error!'
             self.display.setText(result)
         elif key == 'C':
-            self.display.setText('')
-        elif key in constantList:
-            if key == 'pi':
-                key = '3.141592'
-            elif key == '빛의 이동 속도 (m/s)':
-                key = '3E+8'
-            elif key == '소리의 이동 속도 (m/s)':
-                key = '340'
-            elif key == '태양과의 평균 거리 (km)':
-                key = '1.5E+8'
-            self.display.setText(self.display.text() + key)
-        elif key in functionList[0]:
+            self.display.clear()
+        elif key == constantList[0]:
+            self.display.setText(self.display.text() + '3.141592')
+        elif key == constantList[1]:
+            self.display.setText(self.display.text() + '3E+8')
+        elif key == constantList[2]:
+            self.display.setText(self.display.text() + '340')
+        elif key == constantList[3]:
+            self.display.setText(self.display.text() + '1.5E+8')
+        elif key == functionList[0]:
             n = self.display.text()
-            value = factorial(n)
+            value = calcFunctions.factorial(n)
             self.display.setText(str(value))
-        elif key in functionList[1]:
+        elif key == functionList[1]:
             n = self.display.text()
-            value = toBinary(n)
+            value = calcFunctions.decToBin(n)
             self.display.setText(str(value))
-        elif key in functionList[2]:
+        elif key == functionList[2]:
             n = self.display.text()
-            value = toDec(n)
+            value = calcFunctions.binToDec(n)
+            self.display.setText(str(value))
+        elif key == functionList[3]:
+            n = self.display.text()
+            value = calcFunctions.decToRoman(n)
             self.display.setText(str(value))
         else:
             self.display.setText(self.display.text() + key)
-        self.repaint()
 
 
 if __name__ == '__main__':
+
     import sys
 
     app = QApplication(sys.argv)
