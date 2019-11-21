a66988d87f4a3a4b1febc1018699d3094c906250
leeminkyu97 <bomblmk97@naver.com>
2019-11-21 17:47:37 +0900

    몇개 입력하다보면 오류

--- Hangman/guess.py
+++ Hangman/guess.py
@@ -1,12 +1,26 @@
 class Guess:
 
     def __init__(self, word):
+        self.word = word
+        self.numTries = 0
+        self.guessedChars = []
+        self.secretWord = ""
+        self.currentStatus = "_" * len(self.word)
         pass
 
 
     def display(self):
-        pass
+        print("Current:", self.currentStatus)
+        print("Tries:", self.numTries)
 
 
     def guess(self, character):
-        pass
+        self.guessedChars.append(character)
+
+        flag = self.word.find(character)
+        if flag == -1:
+            self.numTries += 1
+            return False
+
+        self.currentStatus[flag] = character
+        return True
